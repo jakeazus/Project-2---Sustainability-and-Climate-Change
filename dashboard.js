915d55929535e5c977bea35b3d2bb3e432a61e5f
Anna Bowers <eenabow@gmail.com>
2021-03-18 19:47:02 -0400

    Adding examples of charts we might use

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+
+// EXAMPLE: Pie chart code with user input: https://www.d3-graph-gallery.com/graph/pie_changeData.html
+
+// EXAMPLE: Multi-line chart with user input and hovering tooltips: https://bl.ocks.org/LemoNode/a9dc1a454fdc80ff2a738a9990935e9d
+
+// Example: Choropleth of world- need to add in geojson to outline countries: https://leafletjs.com/examples/choropleth/
+
