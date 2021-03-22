d753be0899861ec11f3879217a5989eb5acf21e5
Anna Bowers <eenabow@gmail.com>
2021-03-21 22:47:24 -0400

    Adding dashboard example code and potential imagery for navbar

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,9 @@
 
 // EXAMPLE: Pie chart code with user input: https://www.d3-graph-gallery.com/graph/pie_changeData.html
 
-// EXAMPLE: Multi-line chart with user input and hovering tooltips: https://bl.ocks.org/LemoNode/a9dc1a454fdc80ff2a738a9990935e9d
+// EXAMPLE: Bubble chart code with user input: https://www.d3-graph-gallery.com/graph/bubble_tooltip.html
 
-// Example: Choropleth of world- need to add in geojson to outline countries: https://leafletjs.com/examples/choropleth/
+// EXAMPLE: Bargraph code with user input: https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+
+
 
