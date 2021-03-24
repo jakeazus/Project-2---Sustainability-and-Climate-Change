cb2606d0616f3e91ca989be7903fead204b1ac5f
Mario Lozano <Mario@m.lozano1733.com>
2021-03-23 21:59:38 -0400

    updated app.py file

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
+from sqlalchemy import create_engine
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+connection_string = "postgres:postgres@localhost:5432/climate_change_db"
+engine = create_engine(f'postgresql://{connection_string}')
+
+connection = engine.connect()
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 
 
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/names")
+def names():
+
+    result = connection.execute("select * from carbon_dioxide")
+    for row in result:
+        print(row)
 
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
