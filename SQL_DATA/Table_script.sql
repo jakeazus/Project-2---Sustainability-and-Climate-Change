e0d8b10764ab95c3164d19c25efea621209b27b9
Anna Bowers <eenabow@gmail.com>
2021-03-29 21:29:16 -0400

    pushing updated table schema

--- SQL_DATA/Table_script.sql
+++ SQL_DATA/Table_script.sql
@@ -1,27 +1,6 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "carbon_dioxide" (
-	"country_code" VARCHAR   NOT NULL,
-    "country_name" VARCHAR   NOT NULL,
-    "indicator_name" VARCHAR   NOT NULL,
-    "2000" NUMERIC   NOT NULL,
-    "2001" NUMERIC   NOT NULL,
-    "2002" NUMERIC   NOT NULL,
-    "2003" NUMERIC   NOT NULL,
-    "2004" NUMERIC   NOT NULL,
-    "2005" NUMERIC   NOT NULL,
-    "2006" NUMERIC   NOT NULL,
-    "2007" NUMERIC   NOT NULL,
-    "2008" NUMERIC   NOT NULL,
-    "2009" NUMERIC   NOT NULL,
-    "2010" NUMERIC   NOT NULL,
-    "2011" NUMERIC   NOT NULL,
-    "2012" NUMERIC   NOT NULL,
-    "2013" NUMERIC   NOT NULL,
-    "2014" NUMERIC   NOT NULL,
-    "2015" NUMERIC   NOT NULL
-    );
 
 CREATE TABLE "carbon_dioxide_top20" (
 	"country_code" VARCHAR   NOT NULL,
@@ -68,30 +47,10 @@ CREATE TABLE "gdp" (
     "2012" NUMERIC   NOT NULL,
     "2013" NUMERIC   NOT NULL,
     "2014" NUMERIC   NOT NULL,
-    "2015" NUMERIC   NOT NULL
+    "2015" NUMERIC   NOT NULL,
+    "color" VARCHAR NOT NULL
 	);
 
-CREATE TABLE "urban_population" (
-    "country_code" VARCHAR   NOT NULL,
-    "country_name" VARCHAR   NOT NULL,
-    "indicator_name" VARCHAR   NOT NULL,
-    "2000" NUMERIC   NOT NULL,
-    "2001" NUMERIC   NOT NULL,
-    "2002" NUMERIC   NOT NULL,
-    "2003" NUMERIC   NOT NULL,
-    "2004" NUMERIC   NOT NULL,
-    "2005" NUMERIC   NOT NULL,
-    "2006" NUMERIC   NOT NULL,
-    "2007" NUMERIC   NOT NULL,
-    "2008" NUMERIC   NOT NULL,
-    "2009" NUMERIC   NOT NULL,
-    "2010" NUMERIC   NOT NULL,
-    "2011" NUMERIC   NOT NULL,
-    "2012" NUMERIC   NOT NULL,
-    "2013" NUMERIC   NOT NULL,
-    "2014" NUMERIC   NOT NULL,
-    "2015" NUMERIC   NOT NULL
-	);
 
 CREATE TABLE "renewable_energy" (
    "country_code" VARCHAR   NOT NULL,
@@ -112,6 +71,7 @@ CREATE TABLE "renewable_energy" (
     "2012" NUMERIC   NOT NULL,
     "2013" NUMERIC   NOT NULL,
     "2014" NUMERIC   NOT NULL,
-    "2015" NUMERIC   NOT NULL
+    "2015" NUMERIC   NOT NULL,
+    "color" VARCHAR NOT NULL
 );
 
